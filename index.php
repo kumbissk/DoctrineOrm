51d38514682668fac18c587873ff715a3d7bdf43
kumbissk <ramataka81@gmail.com>
2022-04-04 23:22:09 +0200

    ajout de nuvelle fonctionnalites

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require_once "config/autoload.php";
+
+$mvc= new libs\system\BootStrap();
+?>
---
+++ src/controller/ProprietaireController.php
@@ -0,0 +1,82 @@
+<?php
+//namespace src\controller;
+use libs\system\Controller;
+use src\model\ProprietaireDb;
+
+class ProprietaireController extends Controller
+{
+   private $proprietairedao;
+   public function __construct() {
+       parent::__construct();
+       $this->proprietairedao = new ProprietaireDb();
+   }
+  
+   public function ajout(){
+
+      $proprietaire = new Proprietaire();
+      $proprietaire->setNom($_POST['nom']);
+      $proprietaire->setPrenom($_POST['prenom']);
+      $proprietaire->setCivilite($_POST['civilite']);
+      $proprietaire->setDateNaissance($_POST['dateNaissance']);
+      $proprietaire->setLieuNaissance($_POST['lieuNaissance']);
+      $proprietaire->setCni($_POST['cni']);
+      $proprietaire->setAdresse($_POST['adresse']);
+      $proprietaire->setNationalite($_POST['nationalite']);
+      $proprietaire->setContact($_POST['contact']);
+      $proprietaire->setEmail($_POST['email']);
+      $proprietaire->setPassword($_POST['password']);
+      
+      $this->proprietairedao->insert($proprietaire);
+      return $this->list();
+    }
+
+    public function formulaire(){
+      return $this->view->load("Proprietaire/ajout");
+    }
+
+    public function list(){
+        
+        $listes = $this->proprietairedao->finAll();
+        
+        return $this->view->load("Proprietaire/list",$listes);
+        }
+   /**
+    * http://localhost:8081/projects/lpgl/tpmvc/tpORM/Roles/edit/12
+    */
+   public function edit($id) {
+      $trouver = $this->proprietairedao->editBd($id);
+      
+      return $this->view->load("Proprietaire/modification",$trouver);
+   }
+
+   public function details($id) {
+      $trouver = $this->proprietairedao->editBd($id);
+      
+      return $this->view->load("Proprietaire/details",$trouver);
+   }
+
+   public function modifier($id) {
+      $data = $this->proprietairedao->editBd($id);
+      $data->setNom($_POST['nom']);
+      $data->setPrenom($_POST['prenom']);
+      $data->setCivilite($_POST['civilite']);
+      $data->setDateNaissance($_POST['dateNaissance']);
+      $data->setLieuNaissance($_POST['lieuNaissance']);
+      $data->setCni($_POST['cni']);
+      $data->setAdresse($_POST['adresse']);
+      $data->setNationalite($_POST['nationalite']);
+      $data->setContact($_POST['contact']);
+      $data->setEmail($_POST['email']);
+      $data->setPassword($_POST['password']);
+      $this->proprietairedao->modifierBd();
+      return $this->list();
+   }
+   
+   public function delete($id) {
+      $this->proprietairedao->deleteBd($id);
+      return $this->list();
+   }
+
+}
+
+?>
